dad32761562a16f9e1a1b4cd782c67ed87595e3e
Gene Tenorlas <gene.tenorlas@yahoo.com>
2023-01-25 14:06:16 +0000

    Implement all tests and the method

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,18 @@
 class StringCalculator
+  # def self.add(input)
+  #   if input.empty?
+  #     0
+  #   else
+  #     input.to_i
+  #   end
+  # end
+
+  def self.add(input)
+    if input.empty?
+      0
+    else
+      numbers = input.split(",").map { |num| num.to_i }
+      numbers.inject(0) { |sum, number| sum + number }
+    end
+  end
 end
